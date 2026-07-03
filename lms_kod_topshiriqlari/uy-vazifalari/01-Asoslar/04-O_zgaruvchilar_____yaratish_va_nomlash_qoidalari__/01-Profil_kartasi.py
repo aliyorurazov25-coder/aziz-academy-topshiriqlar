70ba4105b975f5c4349126d50293aea6a9029d07
aliyorurazov25-coder <aliyorurazov25@gmail.com>
2026-07-03 08:17:59 +0500

    Add: Profil kartasi [aliyorurazov25-coder]

---
+++ lms_kod_topshiriqlari/uy-vazifalari/01-Asoslar/04-O_zgaruvchilar_____yaratish_va_nomlash_qoidalari__/01-Profil_kartasi.py
@@ -0,0 +1,10 @@
+ism = "Aziz" 
+familya = "Karimov" 
+kurs = "Python" 
+daraja = "Boshlangich" 
+shahar = "Toshkent" 
+
+print("Ism: {ism}") 
+print("Familya: {familya}") 
+print("Daraja: {daraja}") 
+print("Shahar: {shahar}")
