8359ec1ff7b2af428a60e88ccbba9fa93ef59d8a
aliyorurazov25-coder <aliyorurazov25@gmail.com>
2026-07-03 08:09:37 +0500

    Add: snake_case nomlash qoidasi [aliyorurazov25-coder]

---
+++ lms_kod_topshiriqlari/uy-vazifalari/01-Asoslar/04-O_zgaruvchilar_____yaratish_va_nomlash_qoidalari__/01-snake_case_nomlash_qoidasi.py
@@ -0,0 +1,7 @@
+talaba_ismi = "Ali Valiyev" 
+tugilgan_yili = "2005" 
+shahar_nomi = "Toshkent" 
+
+print("Talaba_nomi: {talaba_nomi}") 
+print("Tugilgan_yili: {tugilgan_yili}") 
+print("SHahar_nomi: {shahar_nomi}")
